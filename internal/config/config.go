2b11a537cd266d0aa94c0d80742f5a7b64c68409
dallyger <v.neubauer@darlor.de>
2024-03-24 16:27:24 +0100

    feat(config): facades to access auth data

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,71 @@
 package config
 
 import (
+	"dallyger/telegram-send/internal/telegram"
+	"errors"
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
+var config *Config
+
+type Config struct {
+	auth *viper.Viper
+}
+
+func GetConfig() (*Config, error) {
+
+	if config != nil {
+		return config, nil
+	}
+
+	auth, err := Auth()
+	if err != nil {
+		return nil, err
+	}
+
+	config = &Config {
+		auth: auth,
+	}
+
+	return config, nil
+}
+
+func (c Config) GetBot(botAlias string) (telegram.Bot, error) {
+	if botAlias == "" {
+		botAlias = "default"
+	}
+
+	bot := telegram.Bot {
+		Id: c.auth.GetString(fmt.Sprintf("bot.%s", botAlias)),
+	}
+
+	if bot.Id == "" {
+		return bot, errors.New(fmt.Sprintf("Bot [%s] is missing the auth token", botAlias))
+	}
+
+	return bot, nil
+}
+
+func (c Config) GetChat(chatAlias string) (telegram.Receiver, error) {
+	if chatAlias == "" {
+		chatAlias = "default"
+	}
+
+	id := telegram.Receiver(c.auth.GetString(fmt.Sprintf("chat.%s", chatAlias)))
+
+	if id == "" {
+		return id, errors.New(fmt.Sprintf("Chat [%s] is unknown", chatAlias))
+	}
+
+	return id, nil
+}
+
 func Auth() (*viper.Viper, error) {
+
+	// TODO: make internal once no component relies on it anymore.
+
 	v := viper.New()
 
 	v.SetConfigName("auth.toml")
